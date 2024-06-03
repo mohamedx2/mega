5be07b32090a41431469382c033ea9ea62453486
mohamedx2 <hamroun1969@gmail.com>
2024-06-03 20:19:12 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -62,7 +62,7 @@ const App = () => {
 
 
         <Route path="/" element={getRouteElement()} >
-          
+          <Route path="/chat" element={<Chat />} />
           <Route index element={<Dash_Home />} /> 
           <Route path="/user" element={<User />} /> 
           <Route path="/calendar" element={<Calendar />} /> 
